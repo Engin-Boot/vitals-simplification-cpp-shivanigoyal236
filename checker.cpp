725eb48663d35afaedb7c8322c4e5837f57f27cd
shivanigoyal <38599629+shivanigoyal236@users.noreply.github.com>
2020-08-27 12:23:15 +0530

    Update checker.cpp

--- checker.cpp
+++ checker.cpp
@@ -18,10 +18,10 @@ bool vitalsAreOk(float bpm, float spo2, float respRate) {
 }
 
 int main() {
-  
+   assert(isInsideLimits(30,70,150)==false);
+  assert(isInsideLimits(90,70,150)==true);
   assert(vitalsAreOk(80, 95, 60) == true);
   assert(vitalsAreOk(60, 90, 40) == false);
-  assert(isInsideLimits(30,70,150)==false);
-  assert(isInsideLimits(90,70,150)==true);
+ 
 
 }
