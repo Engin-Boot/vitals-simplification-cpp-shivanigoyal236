e9adc39b1a9ae0d80ddef49c42a1cc13c20b852b
shivanigoyal <38599629+shivanigoyal236@users.noreply.github.com>
2020-08-27 11:13:51 +0530

    Update checker.cpp

--- checker.cpp
+++ checker.cpp
@@ -1,14 +1,14 @@
 #include <assert.h>
 bool is_bpmokay(float bpm)
 {
-  return (bpm < 70 || bpm > 150);
+  return !(bpm < 70 || bpm > 150);
 }
 bool is_spo2okay(float spo2)
 {
-  return (spo2 < 90);}
+  return !(spo2 < 90);}
 bool is_resprateokay(float respRate)
 {
-  return (respRate < 30 || respRate > 95);}
+  return !(respRate < 30 || respRate > 95);}
 bool vitalsAreOk(float bpm, float spo2, float respRate) {
   return (is_bpmokay)&&(is_spo2okay)&&(is_resprateokay);
 }
